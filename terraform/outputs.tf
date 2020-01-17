38044c3cb358cb0cf409c4ad1ed37c8f2205fd81
Chris <chris.ruegger@gmail.com>
2020-01-17 17:34:17 -0500

    modified output variable names

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,9 +6,9 @@ output "sensor_data_queue_arn" {
     value = "${module.queue_sensor_data.queue_arn}"
 }
 
-output "sensor_data_queue_dl_url" {
-    value = "${module.queue_sensor_data.queue_id}"
+output "sensor_data_queue_deadletter_url" {
+    value = "${module.queue_sensor_data.deadletter_queue_id}"
 }
-output "sensor_data_queue_dl_arn" {
-    value = "${module.queue_sensor_data.queue_arn}"
+output "sensor_data_queue_deadletter_arn" {
+    value = "${module.queue_sensor_data.deadletter_queue_arn}"
 }
