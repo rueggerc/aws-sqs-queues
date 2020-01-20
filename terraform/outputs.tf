d383ef138a118e403fb4ddd88e97a23c9aa6fed2
Chris <chris.ruegger@gmail.com>
2020-01-20 04:57:16 -0500

    outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,7 @@
 
+##########################
+# Sensor Data
+##########################
 output "sensor_data_queue_url" {
     value = "${module.queue_sensor_data.queue_id}"
 }
@@ -11,4 +14,39 @@ output "sensor_data_queue_deadletter_url" {
 }
 output "sensor_data_queue_deadletter_arn" {
     value = "${module.queue_sensor_data.deadletter_queue_arn}"
+}
+
+##########################
+# Response 1
+##########################
+output "response1_queue_url" {
+    value = "${module.queue_response1.queue_id}"
+}
+output "response1_queue_arn" {
+    value = "${module.queue_response1.queue_arn}"
+}
+
+output "response1_queue_deadletter_url" {
+    value = "${module.queue_response1.deadletter_queue_id}"
+}
+
+output "response1_queue_deadletter_arn" {
+    value = "${module.queue_response1.deadletter_queue_arn}"
+}
+
+##########################
+# Response 2
+##########################
+output "response2_queue_url" {
+    value = "${module.queue_response2.queue_id}"
+}
+output "response2_queue_arn" {
+    value = "${module.queue_response2.queue_arn}"
+}
+
+output "response2_queue_deadletter_url" {
+    value = "${module.queue_response2.deadletter_queue_id}"
+}
+output "response2_queue_deadletter_arn" {
+    value = "${module.queue_response2.deadletter_queue_arn}"
 }
